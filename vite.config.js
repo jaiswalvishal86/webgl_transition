a706e32c35fbf5f7b6080e51eca266e38d233855
Vishal Jaiswal <vishal@Vishals-MacBook-Air.local>
2024-01-27 12:36:58 +0530

    fixed build script

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import glsl from 'vite-plugin-glsl'
+import glsl from "vite-plugin-glsl";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+export default defineConfig({
+  envDir: __dirname,
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true, // Open to local network and display URL
+    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        page: resolve(__dirname, "src/page/work.html"),
+      },
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
-    },
-    plugins:
-    [
-        glsl()
-    ]
-}
+    outDir: "../dist", // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+  },
+  plugins: [glsl()],
+});
